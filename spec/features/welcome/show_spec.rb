2932772cc6215d640386fa82f8749056f0551ef7
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-04 10:26:59 -0700

    Update welcome show_spec with test for content in navbar

--- spec/features/welcome/show_spec.rb
+++ spec/features/welcome/show_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe 'Site Navigation' do
 
     end
 
+    it "should see a navbar with links to these pages: music, D&D, DJ, music blog, college movie, login, login with Google" do
+      expect(page).to have_link("My Music")
+      expect(page).to have_link("My D&D Game I Run")
+      expect(page).to have_link("My Radio Show")
+      expect(page).to have_link("My Old Music/Movie Blog")
+      expect(page).to have_link("Login without Google Oath")
+      expect(page).to have_link("Login with Google Oath")
+    end
+
 
   end
 end
