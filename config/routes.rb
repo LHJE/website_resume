df0cfa2602b63b892e1767485f4c31894f9d5ddf
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-05 13:25:20 -0700

    Update routes with login post path

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
 
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
 end
