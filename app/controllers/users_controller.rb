3f8605b530616ffd0389ac5f20381b8e351afdf2
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-05 13:52:52 -0700

    Update users controller with create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,18 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}!"
+      redirect_to profile_path
+    else
+      generate_flash(@user)
+      render :new
+    end
+  end
+
   def update
     @user = current_user
     if @user.update(user_params)
