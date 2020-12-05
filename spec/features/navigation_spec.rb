ee71776788fd33f4615a81a1e5fb453bc6a6c33f
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-05 12:18:23 -0700

    Add test to navbar spec, testing if Home link works

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
     describe 'I see a nav bar where I can link to' do
+      it 'the welcome page' do
+        visit music_path
 
+        within 'nav' do
+          click_link 'Home'
+        end
+
+        expect(current_path).to eq(root_path)
+      end
     end
   end
 end
