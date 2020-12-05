7383198c1127faa2fa119025173dbef689542f5c
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-05 12:37:54 -0700

    Update routes with registration path

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  get 'music', to: 'music#index'
+  get '/music', to: 'music#index'
+
+  get '/registration', to: 'users#new', as: :registration
 end
