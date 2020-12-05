63a77c1f7cbd803895c8f9b0b1b5146b6894a1a5
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-05 13:25:27 -0700

    Add a login method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,16 @@ class SessionsController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      login_redirect(user)
+    else
+      flash[:notice] = 'Your email or password was incorrect!'
+      render :new
+    end
+  end
+
   def logout
     session.delete(:user_id)
     session.delete(:cart)
