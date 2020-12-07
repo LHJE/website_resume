99198240dc7bced7643686b0342f9690c0bc14b6
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-06 20:19:13 -0700

    Update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     lat_and_long = SearchFacade.find_lat_and_long("denver, co")
     forecast = WeatherFacade.get_current_forecast(lat_and_long[:lat], lat_and_long[:lng])
-    @image = ImageFacade.get_image(forecast[:weather][0][:main], params[:location])
+    @image = ImageFacade.get_image(forecast.current_weather, lat_and_long)
   end
 end
