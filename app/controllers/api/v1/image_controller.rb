6f8832f1321a46635ee993c39134ecead7dcb9e9
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-06 20:19:30 -0700

    Update image controller (again, just for examples)

--- app/controllers/api/v1/image_controller.rb
+++ app/controllers/api/v1/image_controller.rb
@@ -2,9 +2,9 @@
 class Api::V1::ImageController < ApplicationController
 
   def show
-    lat_and_long = SearchFacade.find_lat_and_long(params[:location])
+    lat_and_long = SearchFacade.find_lat_and_long("denver, co")
     forecast = WeatherFacade.get_current_forecast(lat_and_long[:lat], lat_and_long[:lng])
-    image = ImageFacade.get_image(forecast[:weather][0][:main], params[:location])
+    image = ImageFacade.get_image(forecast.current_weather, lat_and_long)
     render json: ImageSerializer.new(image), status: 200
   end
 
