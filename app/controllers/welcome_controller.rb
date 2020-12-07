606177117e82f844e1dcb6cc167bed01117d7dfc
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-06 19:46:26 -0700

    Update welcome controller to have access to an image for the background

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    lat_and_long = SearchFacade.find_lat_and_long("denver, co")
+    forecast = WeatherFacade.get_current_forecast(lat_and_long[:lat], lat_and_long[:lng])
+    @image = ImageFacade.get_image(forecast[:weather][0][:main], params[:location])
+  end
 end
