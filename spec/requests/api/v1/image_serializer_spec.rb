90510c8b82a6a4389b41097e14a45ce397f1266d
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-06 20:18:59 -0700

    update image serilizer spec

--- spec/requests/api/v1/image_serializer_spec.rb
+++ spec/requests/api/v1/image_serializer_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe ImageSerializer do
     expect(rsp[:data][:type]).to eq("image")
 
     expect(rsp[:data][:attributes]).to be_a(Hash)
-    expect(rsp[:data][:attributes][:location]).to be_a(String)
+    expect(rsp[:data][:attributes][:location]).to be_a(Hash)
     expect(rsp[:data][:attributes][:image_url]).to be_a(String)
     expect(rsp[:data][:attributes][:dimensions]).to be_a(Hash)
     expect(rsp[:data][:attributes][:dimensions][:width]).to be_a(Numeric)
