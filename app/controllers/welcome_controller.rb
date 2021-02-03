19eb72a101e115b59c6acbc56fbd8ed26f903d73
LHJE <theeluke@gmail.com>
2021-02-03 16:56:39 -0700

    Remove API call from welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    lat_and_long = SearchFacade.find_lat_and_long("denver, co")
-    forecast = WeatherFacade.get_current_forecast(lat_and_long[:lat], lat_and_long[:lng])
-    @background_image = ImageFacade.get_image(forecast.current_weather, lat_and_long)
     @profile_images = [1,2,3,4,5]
   end
 end
-
-
-
-
-#
