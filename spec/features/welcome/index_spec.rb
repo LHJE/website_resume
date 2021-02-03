4c5ebd16a265f1cd4929842cbd6845c8dff27f57
LHJE <theeluke@gmail.com>
2021-02-03 15:54:06 -0700

    Update welcome index spec to expect changes to welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,8 +8,12 @@ RSpec.describe 'Welcome Page' do
     end
 
     it "can see welcome page text" do
-      expect(page).to have_content("LUKE HUNTER JAMES-ERICKSON")
-      expect(page).to have_content("oh, hello there")
+      expect(page).to have_content("Luke Hunter James-Erickson")
+      expect(page).to have_content("Ruby on Rails | TDD | Conflict Management")
+      expect(page).to have_link("Let's Chat")
+      expect(page).to have_link("My Solar Garden")
+      expect(page).to have_link("Eras")
+      expect(page).to have_link("Sweater Weather")
     end
   end
 end
